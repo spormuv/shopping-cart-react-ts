a0022bc05c726e6b4fd5088389ade8081ff1c916
Mike <spormuv@gmail.com>
2023-07-06 14:35:49 +0300

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <h1>Shopping Cart</h1>;
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Outlet />
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import About from './pages/About.tsx';
+import Home from './pages/Home.tsx';
+import Store from './pages/Store.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>Something went wrong...</div>,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/store',
+        element: <Store />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
