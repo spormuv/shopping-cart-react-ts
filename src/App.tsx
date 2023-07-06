744da90e422c895b56da48bf6bd21d5090479b37
Mike <spormuv@gmail.com>
2023-07-06 18:26:27 +0300

    feat: add and remove cart items

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
 
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar />
       <Container className="mb-4">
         <Outlet />
       </Container>
-    </>
+    </ShoppingCartProvider>
   );
 }
 
