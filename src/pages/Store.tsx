44d2bcbb37b964417d8d37fd4fbdf6dd0fdf497a
Mike <spormuv@gmail.com>
2023-07-06 15:59:00 +0300

    feat: add store cards

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,20 @@
+import { Col, Row } from 'react-bootstrap';
+import StoreItem from '../components/StoreItem';
+import storeItems from '../data/items.json';
+
 const Store = () => {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Store;
